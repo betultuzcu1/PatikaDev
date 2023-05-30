ab75ec2117d9aa39f0c7b2951c7aa7d28704bc30
Betül TUZCU <betultuzcu1@gmail.com>
2023-05-30 16:25:09 +0300

    mükemmel sayı

---
+++ mukemmler_sayi/src/main/java/org/example/Main.java
@@ -0,0 +1,27 @@
+package org.example;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+     Scanner scanner = new Scanner(System.in);
+
+     System.out.print("Bir sayı girin: ");
+     int number = scanner.nextInt();
+
+     int sum = 0;
+     for (int i = 1; i < number; i++) {
+         if (number % i == 0) {
+             sum += i;
+         }
+     }
+
+     if (sum == number) {
+         System.out.println(number + " mükemmel sayıdır.");
+     } else {
+         System.out.println(number + " mükemmel sayı değildir.");
+     }
+
+    }
+}
+
