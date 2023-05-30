e8a50590d71fe38fe37f11c65d56a5ff7a50dc95
Betül TUZCU <betultuzcu1@gmail.com>
2023-05-30 16:51:25 +0300

    asal sayılar

---
+++ asal_sayi/src/main/java/org/example/Main.java
@@ -0,0 +1,23 @@
+package org.example;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("1-100 arasindaki asal sayilar:");
+
+        for (int number = 2; number <= 100; number++) {
+            boolean isPrime = true;
+
+            for (int i = 2; i < number; i++) {
+                if (number % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime) {
+                System.out.print(number + " ");
+            }
+        }
+    }
+}
